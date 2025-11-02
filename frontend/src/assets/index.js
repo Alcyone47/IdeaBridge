3e9a96617efeb927b1ed698d71084794f8c8945b
Suryanshu <suryanshukumar47@gmail.com>
2025-11-02 15:50:07 +0530

    Landing page layout

---
+++ frontend/src/assets/index.js
@@ -0,0 +1,7 @@
+import rightArrow from "./icons/rightArrow.svg";
+import logo from "./images/temp_logo.svg";
+
+export const images = {
+  logo,
+  rightArrow
+};
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import {images} from "../assets";
+import Button from "../components/common/CustomButton";
+import CustomButton from "../components/common/CustomButton";
 
 const LandingPage = () => {
   return (
-    <div>landingPage</div>
-  )
-}
+    <div className="min-h-screen flex flex-col">
+      <header className="bg-black flex flex-row py-4 px-8">
+        <div className='flex flex-1'>
+          <img src={images.logo} alt="" className='h-12'/>
+        </div>
+        <div className='text-white flex flex-1 items-center justify-center'>
+          <span className="px-6">Product</span>
+          <span className="px-6">Company</span>
+          <span className="px-6">Features</span>
+          <span className="px-6">Marketplace</span>
+        </div>
+        <div className='text-white flex flex-1 items-center justify-end'>
+          <CustomButton text="Sign In" variant="unfilled" className="mr-4" icon={images.rightArrow}/>
+        </div>
+      </header>
+      <main className="flex-1 flex-col items-center justify-center bg-black flex px-4 pb-12">
+          <h1 className="text-white text-4xl md:text-6xl font-bold mb-6 text-center">
+            <span>Welcome to Idea</span>
+            <span className="text-blue-400">Bridge</span>
+          </h1>
+          <p className="text-gray-300 text-lg md:text-xl mb-8 text-center max-w-4xl">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid fuga minima fugiat dolores beatae non quaerat vel dicta ducimus ex.
+          </p>
+          <div className="flex justify-center">
+            <Button text="Get Started" variant="filled" className="font-semibold"/>
+            <Button text="Learn More" variant="unfilled" className="ml-4" icon={images.rightArrow}/>
+        </div>
+      </main>
+    </div>
+  );
+};
 
-export default LandingPage
+export default LandingPage;

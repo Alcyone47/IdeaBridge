91488ad2cbbde4fc5e5043150904fd879e76beb4
Suryanshu <suryanshukumar47@gmail.com>
2025-11-03 09:55:25 +0530

    Added jwt and authMiddleware

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from '../models/userModel.js';
 import bcrypt from 'bcryptjs';
+import generateToken from '../utils/generateToken.js';
 
 export const registerUser = async (req, res) => {
   const { name, email, password, role } = req.body;
@@ -8,7 +9,14 @@ export const registerUser = async (req, res) => {
     if (userExists) return res.status(400).json({ message: 'User already exists' });
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await User.create({ name, email, password: hashedPassword, role });
-    res.status(201).json({ message: 'User registered successfully', user });
+    res.status(201).json({ 
+      message: 'User registered successfully', 
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      token: generateToken(user._id)
+    });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -21,7 +29,14 @@ export const loginUser = async (req, res) => {
     if (!user) return res.status(400).json({ message: 'Invalid email or password' });
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ message: 'Invalid email or password' });
-    res.status(200).json({ message: 'Login successful', user });
+    res.status(200).json({ 
+      message: 'Login successful', 
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      token: generateToken(user._id)
+    });
   } catch (err) {
     res.status(500).json({ message: err.message });
   } 

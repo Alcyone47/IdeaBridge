cd864670319a797bfb45a161370d8d472453edc0
Suryanshu <suryanshukumar47@gmail.com>
2025-11-04 18:08:50 +0530

    Complete entrepreneur dashboard

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,7 +1,9 @@
 import rightArrow from "./icons/rightArrow.svg";
 import logo from "./images/logo-with-text.png";
+import addIcon from "./icons/add-icon.png";
 
 export const images = {
   logo,
-  rightArrow
+  rightArrow,
+  addIcon
 };

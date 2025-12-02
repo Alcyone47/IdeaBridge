e11b9df6e42b51b612cffef9be8f694c223c739d
Suryanshu <suryanshukumar47@gmail.com>
2025-12-02 23:49:08 +0530

    Added investor dashboard

--- frontend/src/pages/dashboards/InvestorDashboard.jsx
+++ frontend/src/pages/dashboards/InvestorDashboard.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import CustomHeader from '../../components/common/CustomHeader'
+import api from '../../api/axios'
+import IdeaDeckCard from '../../components/common/IdeaDeckCard'
+
 
 const InvestorDashboard = () => {
+  const [ideas, setIdeas] = React.useState([])
+  const [loading, setLoading] = React.useState(true); 
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  useEffect(() => {
+    const fetchIdeas = async() => {
+      try {
+        const token = localStorage.getItem("token");
+        const res = await api.get("api/ideas/all", {
+          headers: { Authorization: `Bearer ${token}`}
+        });
+        setIdeas(res.data);
+      }
+      catch(err){
+        console.error("Error fetching all ideas: ", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchIdeas();
+  }, []);
+
   return (
-    <div>InvestorDashboard</div>
+    <div>
+      <CustomHeader/>
+      Investor Dashboard
+    </div>
   )
 }
 
